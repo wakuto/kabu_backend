e5d5150cac698067a8aee013956a04d8f4c2f927
wakuto <waku.902002@gmail.com>
2022-11-23 17:48:21 +0900

    銘柄の更新処理の追加

--- src/brands/brand_code.cjs
+++ src/brands/brand_code.cjs
@@ -1,18 +1,61 @@
-const { Client } = require('pg')
+const pgp = require('pg-promise')();
+const XLSX = require('xlsx');
 
-const pg = new Client({
-  user: 'db_user',
+const pg_conf = {
   host: 'db',
-  database: 'stock_db',
-  password: 'passw0rd',
   port: 5432,
-})
+  database: 'stock_db',
+  user: 'db_user',
+  password: 'passw0rd',
+  max: 30
+};
+
+db = pgp(pg_conf);
+
+console.log("initializing...");
+register_brand()
+  .then( () => {
+    console.log("initialize complete.");
+  });
 
 
-pg.connect()
+// 銘柄情報の登録を行う関数
+async function register_brand() {
+  const url = "https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls"
+  console.log(`data fetch from ${url}`);
+  const data = await (await fetch(url)).arrayBuffer();
 
-pg.query('SELECT * FROM brand_details', (err, result) => {
-  if (err) throw err;
-  console.log(result.rows[0])
-  pg.end()
-})
+  const workbook = XLSX.read(data);
+  const sheet = workbook.Sheets['Sheet1'];
+  const rows = XLSX.utils.sheet_to_json(sheet);
+
+
+  console.log("Reading database...");
+  // dbサーバー負荷軽減のため取得してから検索
+  const brand_codes = await db.any("SELECT brand_code, brand_name FROM brand_details;");
+
+  console.log("Updating database...");
+  for (const row of rows) {
+    const code = row['コード'];
+    const name = row['銘柄名'];
+
+
+    // データを探す
+    const brand_details = brand_codes.filter(result => {
+      return result['brand_code'] === code;
+    });
+
+    // データが見つかったか？
+    if (brand_details.length === 0) {
+      // データが見つからなければinsert
+      console.log(`INSERT: code=${code}, name=${name}`);
+      await db.none("INSERT INTO brand_details(brand_code, brand_name) VALUES($1, $2)", [code, name]);
+    } else {
+      //   データが不一致ならupdate
+      if (brand_details[0]['brand_name'] !== name) {
+        console.log(`UPDATE: code=${code}, name=${name}`);
+        await db.none("UPDATE brand_details SET brand_name=$1 WHERE brand_code=$2", [name, code]);
+      }
+    }
+  }
+}
